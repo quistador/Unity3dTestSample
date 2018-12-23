cee5cd950d3669409272a9bc4855c5eeeb3ed7db
brad <bradsummerquist@hotmail.com>
2018-12-22 18:51:17 -0800

    The test case has an error calling a static function from another assembly.

---
+++ Assets/Scenes/NewBehaviourScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour 
+{
+	/// <summary>
+	/// the stupidest function in the world, 
+	/// used to verify tests.
+	/// </summary>
+	public static int FunctionUnderTest(int a)
+	{
+		return a;
+	}
+}
---
+++ Assets/TestS/NewTestScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class NewTestScript 
+{
+    [Test]
+    public void TestAnotherStaticFunction() 
+    {
+        int a = NewBehaviorScript.FunctionUnderTest(1);
+        int b = 1;
+
+        // Use the Assert class to test conditions.
+        Assert.IsTrue(a == b);
+    }
+}
